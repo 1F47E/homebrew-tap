21b6f1ab704e99518e67d5412ea294bee04ff5ab
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2023-01-29 15:35:46 +0800

    init

---
+++ aespass.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Aespass < Formula
+  desc "A command line tool for encrypting and decrypting text using a password. "
+  homepage "https://github.com/kaspar1ndustries/aes-pass"
+  url "https://github.com/kaspar1ndustries/aes-pass/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "45829e21587221f63e21128b63b5802d48bf52db1e2f0a8de6d29b79d489c45d"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test aespass`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
+
